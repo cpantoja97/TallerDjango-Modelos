02e3af5151d4ea36dcb4fb083d295d4fdddb211c
camilodi1999 <60122434+camilodi1999@users.noreply.github.com>
2021-02-07 18:58:13 -0500

    modelo

--- alarms/models.py
+++ alarms/models.py
@@ -4,6 +4,7 @@ from measurements.models import Measurement
 # Create your models here.
 class Alarm(models.Model):
     measurements = models.ManyToManyField(Measurement)
-    
+    name = models.CharField(max_length=50)
     def __str__(self):
-        return '{}'.format(self.measurements)
+        return '{}'.format(self.name)    
+        
