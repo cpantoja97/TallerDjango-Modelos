ccfecf210e273949374b698a5667b33942c41d27
c.pantoja <camila@tyba.com.co>
2021-02-07 18:33:21 -0500

    Creates Alarm model

--- alarms/models.py
+++ alarms/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from measurements.models import Measurement
 
 # Create your models here.
+class Alarm(models.Model):
+    measurements = models.ManyToManyField(Measurement)
+    
+    def __str__(self):
+        return '{}'.format(self.measurements)
